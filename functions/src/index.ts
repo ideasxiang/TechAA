c71022279a236a27af29acc0f3c8c5765fa99e41
ideasxiang <ideasxiang@gmail.com>
2019-10-23 09:28:21 +0800

    use unique man no

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as functions from 'firebase-functions';
 import admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.database();
-//const ref = db.ref("/TechAuth20191016/ALLUSERS");
+const ref = db.ref("/TechAuth20191016/ALLUSERS");
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -12,14 +12,19 @@ const db = admin.database();
 // });
 
 exports.simpleDbFunction = functions.https.onRequest(async (_,res: any) => {
-    //ref.once("value", function(data) {
+    ref.once("value", function(data) {
       //do some stuff once
-      //const ALLUSERS = data.val();
-      //for (const num of ALLUSERS){
+      let ALLUSERSSTR = data.val();
+      ALLUSERSSTR = ALLUSERSSTR.replace("[","");
+      ALLUSERSSTR = ALLUSERSSTR.replace("]","");
+      const ALLUSERSARRAY = ALLUSERSSTR.split(",");
+      for (const USERNUM of ALLUSERSARRAY){
         //if(typeof(ALLUSERS) === "object"){
-          const REFDAY = db.ref("/TechAuth20191016/12DATEDAY");
-          const REFMONTH = db.ref("/TechAuth20191016/12DATEMONTH");
-          const REFYEAR = db.ref("/TechAuth20191016/12DATEYEAR");
+          const REFDAY = db.ref("/TechAuth20191016/" + `${USERNUM}`+ "DATEDAY");
+          const REFMONTH = db.ref("/TechAuth20191016/" + `${USERNUM}`+ "DATEMONTH");
+          const REFYEAR = db.ref("/TechAuth20191016/" + `${USERNUM}`+ "DATEYEAR");
+          const CURRENTDATE = (new Date()).getTime();
+          enum DATEDUE {No, Soon, Yes};
           REFDAY.once("value",function(day){
             REFMONTH.once("value",function(month){
               REFYEAR.once("value",function(year){
@@ -36,10 +41,21 @@ exports.simpleDbFunction = functions.https.onRequest(async (_,res: any) => {
                 const MONTHARRAY = MONTHSTR.split(",");
                 const YEARARRAY = YEARSTR.split(",");
                 const FULLDATEARRAY = []
+                const DATEDUEARRAY = []
                 let FULLDATESTR = "["
+                let DATEDUESTR = "["
                 for (let num1 = 0; num1 < DAYARRAY.length; num1 ++){
                   //if(typeof(DAYARRAY) === "object" && typeof(MONTHARRAY) === "object" && typeof(YEARARRAY) === "object"){
                     const FULLDATE = (new Date(YEARARRAY[num1] + "-" + MONTHARRAY[num1] + "-" + DAYARRAY[num1] + "T00:00:00")).getTime()
+                    if((FULLDATE-CURRENTDATE)>2592000000){
+                      DATEDUEARRAY.push(DATEDUE.No);
+                    }
+                    else if(2592000000>=(FULLDATE-CURRENTDATE) && (FULLDATE-CURRENTDATE)>0){
+                      DATEDUEARRAY.push(DATEDUE.Soon);
+                    }
+                    else{
+                      DATEDUEARRAY.push(DATEDUE.Yes);
+                    }
                     FULLDATEARRAY.push(FULLDATE)
                   //}
                 }
@@ -48,12 +64,17 @@ exports.simpleDbFunction = functions.https.onRequest(async (_,res: any) => {
                 }
                 FULLDATESTR = FULLDATESTR.slice(0, -1);
                 FULLDATESTR += "]"
-                db.ref("/TechAuth20191016/" + "12" + "FULLDATE").set(FULLDATESTR)
-                .then(res.send("no data")).catch(err => console.log(err));
+                for (const ADUE of DATEDUEARRAY){
+                  DATEDUESTR = DATEDUESTR + `${ADUE}` + ","
+                }
+                DATEDUESTR = DATEDUESTR.slice(0, -1);
+                DATEDUESTR += "]"
+                db.ref("/TechAuth20191016/" + `${USERNUM}`+ "FULLDATE").set(FULLDATESTR).catch(err => console.log(err));
+                db.ref("/TechAuth20191016/" + `${USERNUM}`+ "DATEDUE").set(DATEDUESTR).then(res.send("no data")).catch(err => console.log(err));
               }).catch(err => console.log(err));;
             }).catch(err => console.log(err));;
           }).catch(err => console.log(err));;
-        //};
+        };
       //};
-    //}).catch(err => console.log(err));;
+    }).catch(err => console.log(err));;
   });
